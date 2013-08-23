c95ce34b445d445812f495ca532f9117c943ae43
Chu <mouse170@gmail.com>
2013-08-23 12:16:10 +0800

    Add note

--- test.cpp
+++ test.cpp
@@ -1,16 +1,19 @@
-#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/highgui/highgui.hpp"	//-l opencv_highgui
 
 #include <iostream>
 
 using namespace std;
-using namespace cv;
+using namespace cv;		//opencv library
 
 int main()
 {
     Mat image;
-          image=imread("test.jpg", CV_LOAD_IMAGE_COLOR);  //讀取圖片RGB model
-           namedWindow( "Myimage",CV_WINDOW_AUTOSIZE );  //窗口
-           imshow( "Myimage",image );  //顯示於Myimage 窗口上
-           waitKey(0);  
+          image=imread("test.jpg", CV_LOAD_IMAGE_COLOR);  //read test.jpg and it's property
+          							//CV_LOAD_IMAGE_ANYDEPTH, CV_LOAD_IMAGE_COLOR, CV_LOAD_IMAGE_GRAYSCALE
+
+           namedWindow( "Myimage",CV_WINDOW_AUTOSIZE );  //Window(title, properties)
+
+           imshow( "Myimage",image );  //show image in Myimage
+           waitKey(0);  //delay for 0ms
           return 0;
 }
